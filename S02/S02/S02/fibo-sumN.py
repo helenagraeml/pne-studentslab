ad84e29e6fc77265f75f9f3d1aba6faa960e93b8
helenagr <h.graeml.2025@alumnos.urjc.es>
2026-02-04 17:56:49 +0100

    :)

---
+++ S02/S02/S02/fibo-sumN.py
@@ -0,0 +1,10 @@
+def fibosum(n):
+    a, b = 0, 1
+    lst = []
+    for i in range(n):
+        lst.append(a)
+        a, b = b, a + b
+        i += 1
+    return lst
+print("Sum of fibonaccy sequence of the firt 5th terms is :",sum(fibosum(6)))
+print("Sum of fibonaccy sequence of the firt 10th  terms is :",sum(fibosum(6)))
