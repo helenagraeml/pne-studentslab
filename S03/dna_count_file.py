26a61d10c27566c6d0c174c0be34edc2346ecd84
helenagr <h.graeml.2025@alumnos.urjc.es>
2026-02-11 17:31:54 +0100

    check point

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -2,31 +2,31 @@ from dna_count import count_bases
 
 if __name__ == "__main__":
 
-# WAY 1
-f = open ("dna.txt", "r")
-#here we put the code
-lines = f.readlines()
-f.close() #important to close the terminal
+    # WAY 1
+    # = open ("dna.txt", "r")
+    #here we put the code
+    #lines = f.readlines()
+    #f.close() #important to close the terminal
 
 
-# WAY 2
-with open( "dna.txt", "r") as f:
-    lines = f.readlines()
+    # WAY 2
+    with open( "dna.txt", "r") as f:
+        lines = f.readlines()
 
-total_number = 0
-bases =  {"A": 0 , "C": 0, "G": 0, "T": 0}
-for seq in lines:
-    seq = seq.strip()
+    total_number = 0
+    bases =  {"A": 0 , "C": 0, "G": 0, "T": 0}
 
-    total_number += len(seq)
-    for base in seq:
-        if base in bases:
-            bases[base] += 1
+    for seq in lines:
+        seq = seq.strip()
+        total_number += len(seq)
+        result = count_bases(seq)
+        for key in result:
+            bases[key] += result[key]
 
-print("Total number of bases: ", total_number)
+    print("Total number of bases: ", total_number)
 
 
-for base, count in bases.items():
-    print(f"{base}: {count}")
+    for base, count in bases.items():
+            print(f"{base}: {count}")
 
 
