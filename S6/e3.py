b37fcf30d8568cf79eee451c48a1969d4c6f6bc1
helenagr <h.graeml.2025@alumnos.urjc.es>
2026-02-24 16:38:27 +0100

    check point

---
+++ S6/e3.py
@@ -0,0 +1,41 @@
+class Seq:
+    def __init__(self, bases):
+        valid_base = ["A", "G", "T", "C"]
+        total = 0
+        for i in bases:
+            if i in valid_base:
+                total += 1
+        if total == len(bases):
+            self.bases = bases
+            print("New sequence created!")
+        else:
+            self.bases = "ERROR"
+            print("ERROR!")
+
+    def __str__(self):
+        return self.bases
+
+    def __len__(self):
+        return len(self.bases)
+
+def generate_seqs(pattern, number):
+    lst = []
+    for i in range(1, number + 1):
+        new_seq = Seq(pattern * i)
+        lst.append(new_seq)
+    return lst
+def print_seqs(seq_list):
+    for index, seq in enumerate(seq_list):
+        print(f"Sequence {index}: (Length: {len(seq)}) {seq}")
+
+
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
